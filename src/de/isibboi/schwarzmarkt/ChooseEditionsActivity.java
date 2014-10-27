3200b9f3d9bc068313bf07bd6ebe11023dc13338
Sebastian Schmidt <isibboi@gmail.com>
2014-10-27 19:04:20 +0100

    Added basic Android project layout

---
+++ src/de/isibboi/schwarzmarkt/ChooseEditionsActivity.java
@@ -0,0 +1,14 @@
+package de.isibboi.schwarzmarkt;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ChooseEditionsActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_choose_editions);
+	}
+}
